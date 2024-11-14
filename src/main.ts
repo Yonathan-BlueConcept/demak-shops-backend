ea679fe8e4761f7f5c280b6abab0bcb601cb795c
Yonathan-BlueConcept <64253336+Yonathan-BlueConcept@users.noreply.github.com>
2024-11-14 21:46:23 +0300

    items change

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Memory Usage Logger
+    // setInterval(() => {
+    //   const memoryUsage = process.memoryUsage();
+    //   console.log('Memory Usage:', {
+    //     rss: `${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`,
+    //     heapTotal: `${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB`,
+    //     heapUsed: `${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`,
+    //     external: `${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB`,
+    //   });
+    // }, 5000); // Logs memory usage every 5 seconds
+  
   await app.listen(process.env.PORT || 3015);
 }
 bootstrap();
